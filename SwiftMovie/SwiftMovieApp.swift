c1f04410521cc0092105e55c97bc271258cae20a
= <kumaravi651@gmail.com>
2023-07-24 19:18:43 +0530

    Initial Commit

---
+++ SwiftMovie/SwiftMovieApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftMovieApp.swift
+//  SwiftMovie
+//
+//  Created by Avinash Kumar on 24/07/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftMovieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
